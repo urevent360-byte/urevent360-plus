14a8fe67661b6b43cb571b707d4a792ed81dc5b6
urevent 360 (party rental) <urevent360@gmail.com>
2025-09-12 20:36:28 +0000

    🔧 1. Resolver el error Firebase: Error (auth/configuration-not-found)

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCgx3p7fMTWv-9TJcc32QiKsHRMJmSrBWQ",
   authDomain: "studio-9636239298-2e252.firebaseapp.com",
   projectId: "studio-9636239298-2e252",
-  storageBucket: "studio-9636239298-2e252.firebasestorage.app",
+  storageBucket: "studio-9636239298-2e252.appspot.com",
   messagingSenderId: "298341125021",
   appId: "1:298341125021:web:2e4e6878c1625184d6bd74",
 };

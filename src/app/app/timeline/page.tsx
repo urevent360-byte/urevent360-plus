60b61daf37c2a54ad2c8c1c460f99bb23011347a
urevent 360 (party rental) <urevent360@gmail.com>
2025-09-13 01:40:57 +0000

    File changes

--- src/app/app/timeline/page.tsx
+++ src/app/app/timeline/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function AppTimelinePage() {
     return (
@@ -24,3 +24,4 @@ export default function AppTimelinePage() {
         </div>
     );
 }
+

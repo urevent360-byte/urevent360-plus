58c04c269a05ec2e05702717d186115354004ca8
urevent 360 (party rental) <urevent360@gmail.com>
2025-10-11 00:19:34 +0000

    ./node_modules/firebase/auth/dist/esm/index.esm.js:1:1 Module not found:

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,2 +1,19 @@
 import { app } from "./app";
 export { app };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgx3p7fMTWv-9TJcc32QiKsHRMJmSrBWQ",
+  authDomain: "studio-9636239298-2e252.firebaseapp.com",
+  projectId: "studio-9636239298-2e252",
+  storageBucket: "studio-9636239298-2e252.firebasestorage.app",
+  messagingSenderId: "298341125021",
+  appId: "1:298341125021:web:a0f34866c9cce9d1d6bd74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

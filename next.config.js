7e80b92f948f32bead7d32fbb61126c74c3768f9
urevent 360 (party rental) <urevent360@gmail.com>
2025-10-14 02:24:33 +0000

    Fix next.config images + 404 page; build OK local

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'placehold.co', port: '', pathname: '/**' },
       { protocol: 'https', hostname: 'images.unsplash.com', port: '', pathname: '/**' },
-      { protocol: 'https://', hostname: 'picsum.photos', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', port: '', pathname: '/**' },
     ],
   },
   webpack: (config, { isServer }) => {

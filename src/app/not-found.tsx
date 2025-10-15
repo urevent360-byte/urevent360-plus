99f2dc5c8819d64930bfbb8a942503d60e03b2fd
urevent 360 (party rental) <urevent360@gmail.com>
2025-10-15 20:04:12 +0000

    PUEDES VER QUE ESTA PASANDO

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
+
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center py-24 text-center">
+    <div className="flex flex-grow flex-col items-center justify-center text-center">
       <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
       <p className="text-gray-500">The page you are looking for doesn’t exist.</p>
     </div>

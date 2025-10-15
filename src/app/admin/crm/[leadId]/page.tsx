e44533f720480bfc6200449b2bbb22effa0ab154
urevent 360 (party rental) <urevent360@gmail.com>
2025-10-15 16:35:47 +0000

    Abre src/app/admin/crm/[leadId]/page.tsx y:
    Elimina cualquier import o

--- src/app/admin/crm/[leadId]/page.tsx
+++ src/app/admin/crm/[leadId]/page.tsx
@@ -1,8 +1,11 @@
-
-import * as React from 'react';
 import LeadDetailClient from './client';
 
-export default function AdminLeadDetailPage({ params }: { params: Promise<{ leadId: string }> }) {
-  const { leadId } = React.use(params);
+type Props = {
+  params: { leadId: string };
+  searchParams?: Record<string, string | string[] | undefined>;
+};
+
+export default function AdminLeadDetailPage({ params }: Props) {
+  const { leadId } = params;
   return <LeadDetailClient leadId={leadId} />;
 }

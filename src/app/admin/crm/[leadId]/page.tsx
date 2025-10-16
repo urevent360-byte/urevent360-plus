e7e52a322ff87a78dbdc0e806e32db9addc44e49
urevent 360 (party rental) <urevent360@gmail.com>
2025-10-16 01:41:09 +0000

    Haz un escaneo completo dentro de la carpeta src/app/ buscando estos pat

--- src/app/admin/crm/[leadId]/page.tsx
+++ src/app/admin/crm/[leadId]/page.tsx
@@ -1,17 +1,12 @@
 // src/app/admin/crm/[leadId]/page.tsx
+import LeadDetailClient from './client';
 
 type Props = {
   params: { leadId: string };
   searchParams?: { [key: string]: string | string[] | undefined };
 };
 
-export default async function AdminLeadDetailPage({ params }: Props) {
+export default function AdminLeadDetailPage({ params }: Props) {
   const { leadId } = params;
-  // TODO: add the rest of the page logic here
-  return (
-    <div>
-      <h1>Lead Detail Page</h1>
-      <p>Lead ID: {leadId}</p>
-    </div>
-  );
+  return <LeadDetailClient leadId={leadId} />;
 }

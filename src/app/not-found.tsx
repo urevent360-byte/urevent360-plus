4cf8a0f1bd0e0fcf41b44980c1adc2f1c49d5bd9
urevent 360 (party rental) <urevent360@gmail.com>
2025-10-16 19:01:32 +0000

    File changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 
 export default function NotFound() {
   return (
-    <div className="flex flex-grow flex-col items-center justify-center text-center py-24">
+    <div className="flex flex-grow flex-col items-center justify-center text-center">
       <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
       <p className="text-gray-500">The page you are looking for doesn’t exist.</p>
     </div>

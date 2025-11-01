5b89c273aec96e7952d78b8863bad63c6fc404cd
urevent 360 (party rental) <urevent360@gmail.com>
2025-11-01 22:49:37 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'placehold.co' },
     ],
+  },
+   experimental: {
+    // Required to ignore the 'workspace' directory which causes build slowdowns.
+    exclude: ['**/workspace/**'],
   },
 };
 

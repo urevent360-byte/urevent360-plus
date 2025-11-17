453ce5ee90fe138faa39651219ca78ca4c184524
urevent 360 (party rental) <urevent360@gmail.com>
2025-11-17 02:15:34 +0000

    next.config.js (allowedDevOrigins + picsum)
    Asegúrate de que esté así (

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-      { protocol: 'https', hostname: 'picsum.photos' },
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
       { protocol: 'https', hostname: 'placehold.co' },
     ],
   },
-   experimental: {
+
+  experimental: {
     // Required to ignore the 'workspace' directory which causes build slowdowns.
     exclude: ['**/workspace/**'],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias['genkit'] = false;
+      config.resolve.alias['@genkit-ai/core'] = false;
+      config.resolve.alias['dotprompt'] = false;
+      config.resolve.alias['handlebars'] = false;
+    }
+    return config;
   },
 };
 

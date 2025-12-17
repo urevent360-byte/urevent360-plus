ef9c8a3fa540bef32ff1e4659dac38ba81d2f4a2
urevent 360 (party rental) <urevent360@gmail.com>
2025-12-17 03:42:31 +0000

    mira lo que esta pasando

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 💡 Ignorar errores de ESLint en los builds
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // 💡 Ignorar errores de TypeScript en los builds
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
   // Dominios desde donde puedo cargar imágenes remotas
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https' hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'picsum.photos' },
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
